eaf695579004a5ea97a1c6d9fa129818182b9a04
Dmitry Kudriavtsev <dkudriavtsev@gmail.com>
2020-12-04 21:28:10 -0800

    day 5: actually realized how to solve it

--- days/05/part2.rb
+++ days/05/part2.rb
@@ -6,38 +6,7 @@
 
 input = File.readlines(ARGV[0] || 'input.txt').map(&:chomp)
 
-i = input.map { [ _1[0..6], _1[-3..-1] ] }
-
-ids = i.map { |row, col|
-  high = 0
-  low = 127
-  row.chars.each do |c|
-    temp = (low - high + 1) / 2
-    case c
-    when 'F'
-      low -= temp
-    when 'B'
-      high += temp
-    end
-  end
-  r = high
-
-
-  high = 0
-  low = 7
-  col.chars.each do |c|
-    temp = (low - high + 1) / 2
-    case c
-    when 'R'
-      high += temp
-    when 'L'
-      low -= temp
-    end
-  end
-  c = high
-
-  r * 8 + c
-}
+ids = input.map { _1.tr('FBLR', '0101').to_i(2) }
 
 min = ids.min
 max = ids.max
